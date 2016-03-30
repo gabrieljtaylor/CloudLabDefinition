020edf27e002066fcc9e413d7fd652771acbfc7e
GJTaylor <gabriel.taylor@model-technology.com>
2016-03-30 15:06:46 -0500

    Added MasterDeploymentScript project

---
+++ CloudLabDefinition/MasterDeploymentScript/CloudLab-MasterDeploymentScript.ps1
@@ -0,0 +1,6 @@
+﻿#
+# CloudLab-MasterDeploymentScript.ps1
+# This script is the master script which is used to deploy the collection of resources groups that comprise the cloud lab.
+# Each separate resource group is named here, but defined within RM templates and parameter files, each with its own Deploy script created by VS.
+# This script is used to invoke those other script, enabling a full or piecemeal deployment of the various lab components.
+#
